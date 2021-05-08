994dce480aeed5555267ae9ae598a5464095332a
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-08 22:20:53 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading"; // remember - relative path
+import List from "./List";
+
+const App = function () {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const List = function () {
+  return (
+    <ul>
+      <li>Eat</li>
+      <li>Code</li>
+      <li>Sleep</li>
+    </ul>
+  );
+};
+
+export default List;
